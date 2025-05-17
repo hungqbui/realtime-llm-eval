89221c2b184491ea3cb3d243c5761f9e1ca7a3a9
Hung Bui <quanghungbui8104@gmail.com>
2025-05-16 19:48:22 -0500

    fixed script

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
+# #!/bin/bash
+
 cd ./client && npm run dev &
 cd ./server && python main.py
+
+# Clean up on control-c
+
+trap 'kill $(jobs -p)' EXIT
+# Wait for all background jobs to finish
+wait
