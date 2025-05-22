56af4e45f336626816c5cf282a48f24ef2e197c3
Hung Bui <quanghungbui8104@gmail.com>
2025-05-21 20:47:36 -0500

    add

--- server/models/llm.py
+++ server/models/llm.py
@@ -1,8 +1,8 @@
 from llama_cpp import Llama
 
 llm = Llama.from_pretrained(
-    repo_id="ZiangWu/MobileVLM_V2-1.7B-GGUF",
-    filename="*q8_k.gguf"
+    repo_id="hungqbui/medgemma-4b-it-Q4_K_M-GGUF",
+    filename="*q4_k_m.gguf"
 )
 
 out = llm("What is the capital of France?", max_tokens=10, stop=["Q:", "\n"], echo=True)
