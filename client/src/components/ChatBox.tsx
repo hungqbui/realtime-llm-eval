0ad7e7597b0a56e06df2cb7b274784150b321b63
Hung Bui <quanghungbui8104@gmail.com>
2025-05-27 01:59:36 -0500

    fix

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -13,7 +13,7 @@ function ChatBox(props : any) {
             {props.messages.map((msg : any, index : any) => (
               <div style={{ width: "80%" }} key={index}><div style={{ fontWeight: "bold", width: "80%", display: "flex" }}>{msg.type}</div><span style={{ textAlign: "left", display: "flex", justifyContent: "left" }}>{msg.content}</span></div>
             ))}
-            <div style={{ width: "80%" }}><div style={{ fontWeight: "bold", width: "80%", display: "flex" }}>AI</div><span style={{ textAlign: "left", display: "flex", justifyContent: "left" }}>{props.temp}</span></div>
+            { props.temp && <div style={{ width: "80%" }}><div style={{ fontWeight: "bold", width: "80%", display: "flex" }}>AI</div><span style={{ textAlign: "left", display: "flex", justifyContent: "left" }}>{props.temp}</span></div>}
             <div ref={end}></div>
         </div>
     )
