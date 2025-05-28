14e943e0627fcac913490dcc4ee7e05a54d90529
Hung Bui <quanghungbui8104@gmail.com>
2025-05-27 19:15:48 -0500

    parallel

--- server/models/facial.py
+++ server/models/facial.py
@@ -5,7 +5,7 @@ import torch
 def predict(image):
     processor = AutoImageProcessor.from_pretrained("mo-thecreator/vit-Facial-Expression-Recognition")
     model = ViTForImageClassification.from_pretrained("mo-thecreator/vit-Facial-Expression-Recognition").to("cuda" if torch.cuda.is_available() else "cpu")
-    inputs = processor(images=image, return_tensors="pt")
+    inputs = processor(images=image, return_tensors="pt").to("cuda" if torch.cuda.is_available() else "cpu")
 
     with torch.no_grad():
         logits = model(**inputs, return_dict=True).logits
